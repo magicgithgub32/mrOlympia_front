e18202c6e49d1e60e77ff8a71ea9f8f146d11ea3
Rubcs <Rubcs@gmail.com>
2023-09-26 17:47:25 +0200

    add content to winners page

--- src/pages/Winners/Winners.jsx
+++ src/pages/Winners/Winners.jsx
@@ -7,26 +7,27 @@ import { useState } from "react";
 import { useEffect } from "react";
 
 const Winners = () => {
-  const [allContests, setAllContests] = useState();
+  const [allContests, setAllContests] = useState([]);
 
   useEffect(() => {
     const getData = async () => {
       try {
+        console.log(`${import.meta.env.VITE_API_URL}/contests`);
+
         const res = await fetch(`${import.meta.env.VITE_API_URL}/contests`);
+
         if (!res.ok) {
           throw new Error(`HTTP Error! Status: ${res.status}`);
         }
         const data = await res.json();
+        setAllContests(data);
         return data;
       } catch (error) {
         console.error("Error fetching data:", error);
-        throw error;
       }
     };
-    getData()
-      .then((data) => setAllContests(data))
-      .catch((error) => console.log("Data not found", error));
-  }, [allContests]);
+    getData();
+  }, []);
 
   console.log(import.meta.env.VITE_API_URL);
   console.log(allContests);
@@ -38,7 +39,28 @@ const Winners = () => {
         <Button buttonText={"HOME"} className={"back-home-button"}></Button>
       </Link>
       <div>
-        <p>{allContests[0].year}</p>
+        {allContests.length > 0 ? (
+          <>
+            <ul>
+              {allContests.map((contest) => (
+                <div key={contest.id} className="contest-card-wrapper">
+                  <p>YEAR: {contest.year}</p>
+                  <p>WINNER: {contest.winner[0].name}</p>
+                  <div className="winner-image-wrapper">
+                    <img
+                      className="winner-image"
+                      src={contest.winner[0].image}
+                    />
+                  </div>
+                  <p>NATIONALITY: {contest.winner[0].nationality}</p>
+                  <p>AGE: {contest.age}</p>
+                </div>
+              ))}
+            </ul>
+          </>
+        ) : (
+          <p>Loading...</p>
+        )}
       </div>
     </>
   );
